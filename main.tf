548f0ffcdde6ee36bf1621b4f8d00a923a71e399
Ajin <ajinm@qburst.com>
2022-07-07 02:41:02 +0530

    VPC and security group

--- main.tf
+++ main.tf
@@ -6,7 +6,25 @@ resource "aws_instance" "example" {
 
 ami = "ami-00978328f54e31526"
 instance_type = "t2.micro"
+user_data = <<-EOF
+            #!/bin/bash
+            echo "Hey My Ammukutty" >index.html
+            nohup busybox httpd -f -p 8080 &
+            EOF
+vpc_security_group_ids = [aws_security_group.instance.id]
 tags= {
 Name = "terraform-project"
 }
+}
+
+resource "aws_security_group" "instance" {
+name = "terraform-example"
+ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+
+ 
 }
